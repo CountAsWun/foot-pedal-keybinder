0cd5344b87c8087f0cb7582b240e147ba2ad6cac
CountAsWun <102053753+CountAsWun@users.noreply.github.com>
2026-05-12 09:25:41 -0400

    Add app entry point

---
+++ native/FootPedalKeybinder/Program.cs
@@ -0,0 +1,11 @@
+namespace FootPedalKeybinder;
+
+internal static class Program
+{
+    [STAThread]
+    private static void Main()
+    {
+        ApplicationConfiguration.Initialize();
+        Application.Run(new MainForm());
+    }
+}
